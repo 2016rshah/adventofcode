f8188a45e14aa2cc12d567390c120136681c6f7b
Rushi Shah <2016rshah@gmail.com>
2015-12-05 10:37:46 -0500

    Added comment

--- day1/Floor.hs
+++ day1/Floor.hs
@@ -16,6 +16,7 @@ path curr (p:ps) = nxt : (path nxt ps)
 	where nxt = followInstruction curr p
 path curr _ = []
 
+--Trying to reimplement path with a fold, but I don't know how to keep track of both the current floor number and the list of all floor numbers at the same time
 --path2 :: [P] -> [Int]
 --path2 xs = foldl (\curr acc -> ((followInstruction (last acc) curr) : acc)) [0] xs
 
