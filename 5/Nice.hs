6991b4e8139c74a67b27cb8c174443e318fd0479
Rushi Shah <2016rshah@gmail.com>
2015-12-06 13:45:50 -0500

    Finished both problems on day 5

---
+++ 5/Nice.hs
@@ -0,0 +1,38 @@
+import Data.List
+
+threeVowels :: String -> Bool
+threeVowels s = length vs >= 3
+  where vs = filter (\c -> elem c "aeiou") s
+
+duplicate :: String -> Bool
+duplicate (x:y:zs) = if (x==y) then True else duplicate (y:zs) 
+duplicate _ = False
+
+passesBlacklist :: String -> Bool
+passesBlacklist s = not (foldr (||) False doesContain)
+  where
+    bl = ["ab", "cd", "pq", "xy"]
+    doesContain = map (\e -> isInfixOf e s) bl
+
+isNice1 :: String -> Bool
+isNice1 s = (threeVowels s) && (duplicate s) && (passesBlacklist s)
+
+divorce :: String -> Bool
+divorce (x:y:z:es) = if (x==z) then True else divorce (y:z:es)
+divorce _ = False
+
+twins :: String -> Bool
+twins (x:y:zs) = if isInfixOf [x,y] zs then True else twins (y:zs)
+twins _ = False
+--Caught the fact that I need to call twins on (y:zs) instead of just zs thanks to
+--https://github.com/tylerjl/adventofcode/blob/master/src/Y2015/D05.hs
+
+isNice2 :: String -> Bool
+isNice2 s = twins s && divorce s
+
+main = do
+  inp <- readFile "input.txt"
+  let strings = lines inp
+  print $ (length . filter ((==) True) . map isNice1) strings
+  print $ (length . filter ((==) True) . map isNice2) strings 
+
